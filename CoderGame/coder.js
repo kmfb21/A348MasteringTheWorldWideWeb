3fb7f1d07f0062a06eb8c6d24f7ecb56cce46c8b
kmfb21 <kmfb21@gmail.com>
2016-01-16 10:23:51 -0500

    Coder Game Example by jquery

---
+++ CoderGame/coder.js
@@ -0,0 +1,351 @@
+$(function() {
+
+    var backgroundAnim = new gf.animation({//图片赋值
+        url : "back.png"
+    });
+    var frontAnim = new gf.animation({
+        url: "front.png"
+    });
+    var networkPacketsAnim = new gf.animation({
+        url : "packet.png"
+    });
+    var bugsAnim = new gf.animation({
+        url : "bug.png"
+    });
+    var playerAnim = new gf.animation({
+        url : "player.png"
+    });
+
+    var initialize = function() {//图片初始化
+        $("#mygame").append("<div id='container' style='display: none; width: 640px; height: 480px;'>");
+        gf.addSprite("container","background",{width: 640, height: 480});
+        gf.addSprite("container","packets1",{width: 640, height: 40, y: 400});
+        gf.addSprite("container","packets2",{width: 640, height: 40, y: 330});
+        gf.addSprite("container","packets3",{width: 640, height: 40, y: 260});
+        gf.addSprite("container","bugs1",{width: 640, height: 60, y: 170});
+        gf.addSprite("container","bugs2",{width: 640, height: 60, y: 100});
+        gf.addSprite("container","bugs3",{width: 640, height: 60, y: 30});
+        gf.addSprite("container","front",{width: 640, height: 480});
+        gf.addSprite("container","player",{width: 40, height: 40, y: 440, x: 260});
+
+        gf.setAnimation("background", backgroundAnim);
+        gf.setAnimation("player", playerAnim);
+        gf.setAnimation("packets1", networkPacketsAnim);
+        gf.setAnimation("packets2", networkPacketsAnim);
+        gf.setAnimation("packets3", networkPacketsAnim);
+        gf.setAnimation("bugs1", bugsAnim);
+        gf.setAnimation("bugs2", bugsAnim);
+        gf.setAnimation("bugs3", bugsAnim);
+        gf.setAnimation("front", frontAnim);
+        
+        $("#startButton").remove();
+        $("#container").append("<div id='lifes' style='position: absolute; top: 10px; left: 20px; color: black;'>life: 3</div>").css("display", "block");
+        setInterval(gameLoop, 100);
+    }
+    
+    var screenWidth = 640;
+    var packets1 = {
+        position: 300,
+        speed: 3
+    }
+    var packets2 = {
+        position: 0,
+        speed: -5
+    }
+    var packets3 = {
+        position: 30,
+        speed: 5
+    }
+    var bugs1 = {
+        position: 100,
+        speed: -10
+    }
+    var bugs2 = {
+        position: 0,
+        speed: -6
+    }
+    var bugs3 = {
+        position: 50,
+        speed: -8
+    }
+    
+    var gameState = "START";
+    
+    var gameLoop = function() {
+        packets1.position += packets1.speed;//乱七八糟在走
+        $("#packets1").css("background-position",""+ packets1.position +"px 0px");
+        
+        packets2.position += packets2.speed;
+        $("#packets2").css("background-position",""+ packets2.position +"px 0px");
+        
+        packets3.position += packets3.speed;
+        $("#packets3").css("background-position",""+ packets3.position +"px 0px");
+        
+        bugs1.position += bugs1.speed;
+        $("#bugs1").css("background-position",""+ bugs1.position +"px 0px");
+        bugs2.position += bugs2.speed;
+        $("#bugs2").css("background-position",""+ bugs2.position +"px 0px");
+        bugs3.position += bugs3.speed;
+        $("#bugs3").css("background-position",""+ bugs3.position +"px 0px");
+    
+        var newPos = gf.x("player");//获取横向位置
+		
+        switch(gameState){//跟着走
+            case "LINE1":
+                newPos += packets1.speed;
+                break;
+            case "LINE2":
+                newPos += packets2.speed;
+                break;
+            case "LINE3":
+                newPos += packets3.speed;
+                break;
+        }
+
+        if(newPos > screenWidth || newPos < -40){//不能出屏幕
+                kill();
+        } else {
+            if(!detectSafe(gameState)){
+                kill();
+            }
+            gf.x("player", newPos);
+        }
+    };
+	
+    $(document).keydown(function(e){//按键判断
+        if(gameState != "WON" && gameState != "GAMEOVER"){//如果没有结束的话
+            switch(e.keyCode){
+                case 37: //left
+                    gf.x("player",gf.x("player") - 5);
+                    if(!detectSafe(gameState)){
+                        kill();
+                    }
+                    break;
+                case 39: // right
+                    gf.x("player",gf.x("player") + 5);
+                    if(!detectSafe(gameState)){
+                        kill();
+                    }
+                    break;
+                case 38: // jump
+                    switch(gameState){
+                        case "START":
+                            $("#player").animate({top: 400},function(){//跳到第一行
+                                if(detectSafe("LINE1")){//如果line1安全就赋值
+                                    gameState = "LINE1";
+                                } else {
+                                    kill();
+                                }
+                            });
+                            break;
+                        case "LINE1":
+                            $("#player").animate({top: 330},function(){//跳到第二行
+                                if(detectSafe("LINE2")){
+                                    gameState = "LINE2";
+                                } else {
+                                    kill();
+                                }
+                            });
+                            break;
+                        case "LINE2":
+                            $("#player").animate({top: 260},function(){//跳到第三行
+                                if(detectSafe("LINE3")){
+                                    gameState = "LINE3";
+                                } else {
+                                    kill();
+                                }
+                            });
+                            break;
+                        case "LINE3":
+                            $("#player").animate({top: 220},function(){//跳到休息并且不判断安全
+                                gameState = "REST";
+                            });
+                            break;
+                        case "REST":
+                            $("#player").animate({top: 180},function(){//跳到第四行
+                                if(detectSafe("LINE4")){
+                                    gameState = "LINE4";
+                                } else {
+                                    kill();
+                                }
+                            });
+                            break;
+                        case "LINE4":
+                            $("#player").animate({top: 110},function(){//跳到第五行
+                                if(detectSafe("LINE5")){
+                                    gameState = "LINE5";
+                                } else {
+                                    kill();
+                                }
+                            });
+                            break;
+                        case "LINE5":
+                            $("#player").animate({top: 40},function(){//跳到第六行
+                                if(detectSafe("LINE6")){
+                                    gameState = "LINE6";
+                                } else {
+                                    kill();
+                                }
+                            });
+                            break;
+                        case "LINE6":
+                            $("#player").animate({top: 0},function(){//跳到终点结束
+                                gameState = "WON";
+                                $("#lifes").html("You won!");
+                            });
+                            break;
+                    }
+					break;
+				case 40: //自创
+					switch(gameState){
+						case "LINE1":
+                            $("#player").animate({top: 440},function(){//跳到开始
+                                gameState = "START";
+                            });
+                            break;
+						case "LINE2":
+                            $("#player").animate({top: 400},function(){//跳到第一行
+                                if(detectSafe("LINE1")){//如果line1安全就赋值
+                                    gameState = "LINE1";
+                                } else {
+                                    kill();
+                                }
+                            });
+                            break;
+						case "LINE3":
+                            $("#player").animate({top: 330},function(){//跳到第二行
+                                if(detectSafe("LINE2")){
+                                    gameState = "LINE2";
+                                } else {
+                                    kill();
+                                }
+                            });
+                            break;
+						case "REST":
+                            $("#player").animate({top: 260},function(){//跳到第三行
+                                if(detectSafe("LINE3")){
+                                    gameState = "LINE3";
+                                } else {
+                                    kill();
+                                }
+                            });
+                            break;
+						case "LINE4":
+						    $("#player").animate({top: 220},function(){//跳到休息并且不判断安全
+                                gameState = "REST";
+                            });
+                            break;
+						case "LINE5":
+						$("#player").animate({top: 180},function(){//跳到第四行
+                                if(detectSafe("LINE4")){
+                                    gameState = "LINE4";
+                                } else {
+                                    kill();
+                                }
+                            });
+                            break;
+						case "LINE6":
+						$("#player").animate({top: 110},function(){//跳到第五行
+                                if(detectSafe("LINE5")){
+                                    gameState = "LINE5";
+                                } else {
+                                    kill();
+                                }
+                            });
+                            break;
+					}
+			}
+        }
+    });
+    var detectSafe = function(state){//是否进入危险
+        switch(state){
+            case "LINE1":
+                var relativePosition = (gf.x("player") - packets1.position) % 230;
+                relativePosition = (relativePosition < 0) ? relativePosition + 230: relativePosition;
+                if(relativePosition > 110 && relativePosition < 210) {
+                    return true;
+                } else {
+                    return false;
+                }
+                break;
+            case "LINE2":
+                var relativePosition = (gf.x("player") - packets2.position) % 230;
+                relativePosition = (relativePosition < 0) ? relativePosition + 230: relativePosition;
+                if(relativePosition > 110 && relativePosition < 210) {
+                    return true;
+                } else {
+                    return false;
+                }
+                break;
+            case "LINE3":
+                var relativePosition = (gf.x("player") - packets3.position) % 230;
+                relativePosition = (relativePosition < 0) ? relativePosition + 230: relativePosition;
+                if(relativePosition > 110 && relativePosition < 210) {
+                    return true;
+                } else {
+                    return false;
+                }
+                break;
+            case "LINE4":
+                var relativePosition = (gf.x("player") - bugs1.position) % 190;
+                relativePosition = (relativePosition < 0) ? relativePosition + 190: relativePosition;
+                if(relativePosition < 130) {
+                    return true;
+                } else {
+                    return false;
+                }
+                break;
+            case "LINE5":
+                var relativePosition = (gf.x("player") - bugs2.position) % 190;
+                relativePosition = (relativePosition < 0) ? relativePosition + 190: relativePosition;
+                if(relativePosition < 130) {
+                    return true;
+                } else {
+                    return false;
+                }
+                break;
+            case "LINE6":
+                var relativePosition = (gf.x("player") - bugs3.position) % 190;
+                relativePosition = (relativePosition < 0) ? relativePosition + 190: relativePosition;
+                if(relativePosition < 130) {
+                    return true;
+                } else {
+                    return false;
+                }
+                break;
+        }
+        return true;
+    }
+    var life = 3;//初始生命值
+    var kill = function (){//死亡判断
+        life--;
+        if(life == 0) {
+            gameState = "GAMEOVER";
+            $("#lifes").html("Game Over!");
+        } else {
+            $("#lifes").html("life: "+life);
+            switch(gameState){
+                case "START":
+                case "LINE1":
+                case "LINE2":
+                case "LINE3":
+                    gf.x("player", 260);
+                    gf.y("player", 440);
+                    gameState = "START";
+                    break;
+                case "REST":
+                case "LINE4":
+                case "LINE5":
+                case "LINE6":
+                    gf.x("player", 260);
+                    gf.y("player", 220);
+                    gameState = "REST";
+                    break;
+            }
+        }
+    }
+    
+    $("#startButton").click(function() {//调用初始化
+        gf.startPreloading(initialize);
+    });
+});
